f6cfeb0fe5ce081e7bc1554e557a480115f36337
Clifford T. Matthews <ctm@devctm.com>
2024-09-16 11:14:39 -0600

    includes an examinable button

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+// Here's my plan:
+// Add Adrienne's assets, with the regular button showing.
+//   look up how to do trunk assets
+//   look at how the button is displayed in mb2 so we get the right size
+//   and zoom
+// Make it so that clicking on it brings up the narcoleptic dinosaur
+// Make it so that shift-clicking brings up an image picker
+// Ase an ObjectURL to update the image
+//
+// See https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications
+// for more info
+
+
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello Word" }</h1>
+        <div class={"button"}>
+            <div class={"button-wrapper examine"}/>
+        </div>
     }
 }
 
