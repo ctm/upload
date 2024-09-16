a7bf03e9dbeb063b2930b7219e308a9d7f1246d2
Clifford T. Matthews <ctm@devctm.com>
2024-09-16 12:31:34 -0600

    clicking button gets us the sleeping dino

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-// Here's my plan:
-// Add Adrienne's assets, with the regular button showing.
-//   look up how to do trunk assets
-//   look at how the button is displayed in mb2 so we get the right size
-//   and zoom
-// Make it so that clicking on it brings up the narcoleptic dinosaur
 // Make it so that shift-clicking brings up an image picker
 // Ase an ObjectURL to update the image
 //
 // See https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications
 // for more info
 
-
+use wasm_bindgen::JsCast;
+use web_sys::Element;
 use yew::prelude::*;
+
+const FLIPPED: &str = "flipped";
+
+fn toggle_flipped(e: &MouseEvent) -> Option<()> {
+    let cl = e.target()?.dyn_into::<Element>().ok()?.class_list();
+    if cl.contains(FLIPPED) {
+        let _ = cl.remove_1(FLIPPED);
+    } else {
+        let _ = cl.add_1(FLIPPED);
+    }
+    None
+}
 
 #[function_component(App)]
 fn app() -> Html {
+    let onclick: Callback<MouseEvent> = {
+        |e: MouseEvent| {
+            toggle_flipped(&e);
+        }
+    }
+    .into();
     html! {
         <div class={"button"}>
-            <div class={"button-wrapper examine"}/>
+            <div class={"button-wrapper examine"} {onclick}/>
         </div>
     }
 }
